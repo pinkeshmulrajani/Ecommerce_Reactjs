11fcc1075bc00f9302fcc7a8843efa1127f73628
pinkeshmulrajani <pinkeshmulrajani@gmail.com>
2026-08-20 11:46:57 +0530

    forgot password workable in shop_admin panel

--- shop_admin/src/Auth/forgot_password.js
+++ shop_admin/src/Auth/forgot_password.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getBaseUrl } from '../common';
+import axios from 'axios';
+import { showError, showMessage } from '../message';
+import { ToastContainer } from 'react-toastify';
 
 export default function ForgotPassword() {
+
+	let [email, setEmail] = useState("");
+	let doSendMail = (e) => {
+		e.preventDefault();
+		let apiAddress = getBaseUrl() + "forgot_password.php";
+		let form = new FormData();
+		form.append("email", email);
+		let options = {
+			url: apiAddress,
+			method: 'post',
+			responseType: 'json',
+			data: form
+		};
+		axios(options).then((response) => {
+			console.log(response.data);
+			let error = response.data[0]['error'];
+			if (error !== 'no') {
+				showError(error);
+			} else {
+				let success = response.data[1]['success'];
+				if(success === 'no') {
+					showError(response.data[2]['message']);
+				} else {
+					showMessage(response.data[2]['message']);
+				}
+			}
+		}).catch((error) => {
+			showError();
+		});
+	}
+
 	return (
 		<main className="d-flex w-100 h-100">
+			<ToastContainer limit={1} />
 			<div className="container d-flex flex-column">
 				<div className="row vh-100">
 					<div className="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
@@ -19,7 +55,7 @@ export default function ForgotPassword() {
 												<hr />
 											</div>
 										</div>
-										<form>
+										<form on onSubmit={doSendMail}>
 											<div className="mb-3">
 												<label className="form-label">Email</label>
 												<input className="form-control form-control-lg" type="email" name="email" placeholder="Enter your email" />
