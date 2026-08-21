d30200e6bac3cb1f6934e4b005db76fb579f4e85
pinkeshmulrajani <pinkeshmulrajani@gmail.com>
2026-08-21 12:58:20 +0530

    user login and logout code added but proper not work login and logout in shop_users panel

--- shop_users/src/Auth/login.js
+++ shop_users/src/Auth/login.js
@@ -1,48 +1,119 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Layout/header';
-import Footer from '../Layout/footer';
+import footerr from '../Layout/footer'
+import axios from 'axios';
+import { getBaseUrl } from '../common';
+import { showError, showMessage } from '../message';
+import { ToastContainer } from 'react-toastify';
+import withHooks from '../hoc';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <section className="login footer-padding">
-          <div className="container">
-            <div className="login-section">
-              <div className="review-form" style={{ height: '560px' }}>
-                <h5 className="comment-title">Log In</h5>
-                <div className="review-inner-form">
-                  <div className="review-form-name">
-                    <label htmlFor="email" className="form-label">Email Address**</label>
-                    <input type="email" id="email" className="form-control" placeholder="Email" />
-                  </div>
-                  <div className="review-form-name">
-                    <label htmlFor="password" className="form-label">Password*</label>
-                    <input type="password" id="password" className="form-control" placeholder="Password" />
-                  </div>
-                  <div className="review-form-name checkbox">
-                    <div className="checkbox-item">
-                      <input type="checkbox" />
-                      <span className="address">Remember Me</span>
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    }
+
+    handleLogin = (e) => {
+        e.preventDefault();
+        const { email, password } = this.state;
+
+        if (!email || !password) {
+            showError("Email and Password are required");
+            return;
+        }
+
+        let formData = new FormData();
+        formData.append('email', email);
+        formData.append('password', password);
+
+        let apiAddress = getBaseUrl() + 'login.php';
+        let options = {
+            url: apiAddress,
+            method: 'post',
+            data: formData,
+            responseType: 'json'
+        };
+
+        axios(options).then((response) => {
+            let error = response.data[0]['error'];
+            if (error !== 'no') {
+                showError(error);
+            } else {
+                let userId = response.data[2]['id'];
+                this.props.setCookie('id', userId, { path: '/' });
+                showMessage(response.data[1]['message']);
+                setTimeout(() => {
+                    this.props.navigate('/');
+                }, 2000);
+            }
+        }).catch((error) => {
+            showError();
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <ToastContainer />
+                <Header />
+
+                <section className="login footer-padding">
+                    <div className="container">
+                        <div className="login-section">
+                            <div className="review-form">
+                                <h5 className="comment-title">Log In</h5>
+                                <div className="review-inner-form">
+                                    <div className="review-form-name mb-3">
+                                        <label htmlFor="email" className="form-label">Email Address*</label>
+                                        <input
+                                            type="email"
+                                            id="email"
+                                            className="form-control"
+                                            placeholder="Email"
+                                            value={this.state.email}
+                                            onChange={this.handleInputChange}
+                                        />
+                                    </div>
+                                    <div className="review-form-name mb-3">
+                                        <label htmlFor="password" className="form-label">Password*</label>
+                                        <input
+                                            type="password"
+                                            id="password"
+                                            className="form-control"
+                                            placeholder="Password"
+                                            value={this.state.password}
+                                            onChange={this.handleInputChange}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="d-flex justify-content-between align-items-center mb-3">
+                                    <Link to="/forgotpassword" className="text-decoration-none text-primary">Forgot password?</Link>
+                                </div>
+                                <div className="login-btn text-center">
+                                    <button type="submit" className="shop-btn w-100" onClick={this.handleLogin}>Sign in</button>
+                                </div>
+                                <div className="text-center mt-3">
+                                    <p className="mb-0">Don't have an account? <Link to="/register" className="text-decoration-none text-primary">Sign up</Link></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="forget-pass">
-                      <Link to="/forgot-password">Forgot password?</Link>
-                    </div>
-                  </div>
-                </div>
-                <div className="login-btn text-center">
-                  <button type="submit" className="shop-btn">Log In</button>
-                </div>
-              </div>
+                </section>
+
+                <footer />
             </div>
-          </div>
-        </section>
-        <Footer />
-      </>
-    );
-  }
+        );
+    }
 }
 
-export default Login;
+export default withHooks(Login);
--- shop_users/src/index.js
+++ shop_users/src/index.js
@@ -13,6 +13,8 @@ import Register from './Auth/register';
 import ChangePassword from './Auth/change_password';
 import ForgotPassword from './Auth/forgot_password';
 import { CookiesProvider } from 'react-cookie';
+import logout from './Auth/logout';
+
 function MyRouter() {
     return (
         <CookiesProvider>
@@ -29,6 +31,7 @@ function MyRouter() {
                     <Route path="/changepassword" element={<ChangePassword />} />
                     <Route path="/forgotpassword" element={<ForgotPassword />} />
                     <Route path="/shop" element={<Shop />} />
+                    <Route path="/logout" element={<logout />} />
                 </Routes>
             </Router>
         </CookiesProvider>
