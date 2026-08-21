f2eba68b1e980cacb99ef23665e5ed70111ccdc5
pinkeshmulrajani <pinkeshmulrajani@gmail.com>
2026-08-21 10:53:06 +0530

    get shop form page to load api data in shop page in shop_users panel

--- shop_users/src/Shop/shop.js
+++ shop_users/src/Shop/shop.js
@@ -2,31 +2,73 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Layout/header';
 import Footer from '../Layout/footer';
+import { getBaseUrl, getImageBase } from '../common';
+import axios from 'axios';
+import { showError } from '../message';
+import { ToastContainer } from 'react-toastify';
 
-class Shop extends React.Component {
+export default class Shop extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    }
+  }
+
+  componentDidMount() {
+    let apiAddress = getBaseUrl() + "category.php";
+    let options = {
+      url: apiAddress,
+      method: 'get',
+      responseType: 'json',
+    };
+
+    axios(options).then((response) => {
+      let error = response.data[0]['error'];
+      if (error !== 'no') {
+        showError(error);
+      } else {
+        let total = response.data[1]['total'];
+        if (total === 0) {
+          showError('Categories Not Found.');
+        } else {
+          response.data.splice(0, 2);
+          this.setState({
+            categories: response.data
+          });
+        }
+      }
+    }).catch((error) => {
+      showError(error);
+    });
+  }
+
   render() {
-    const products = [
-      { id: 1, name: 'Tom Checked shirt', price: '$26.99', image: '/shop_users/assets/images/homepage-one/product-img/product-img-5.webp' },
-      { id: 2, name: 'Leather Dress Shoes', price: '$13.99', image: '/shop_users/assets/images/homepage-one/product-img/product-img-5.webp' }
-    ];
-
     return (
       <>
+        <ToastContainer />
         <Header />
-        <section className="product-category my-5">
+        <section className="product-category my-5 footer-padding">
           <div className="container">
             <div className="section-title">
               <h5>Our Categories</h5>
             </div>
             <div className="category-section">
-              <div className="product-wrapper" data-aos="fade-right" data-aos-duration="100">
-                <div className="wrapper-img">
-                  <img src="/shop_users/assets/images/homepage-one/category-img/dresses.webp" alt="dress" />
-                </div>
-                <div className="wrapper-info">
-                  <Link to="/category-product" className="wrapper-details">Dresses</Link>
-                </div>
-              </div>
+              {this.state.categories.map((item) => {
+                return (
+                  <Link to={"/product/" + item.id} className='wrapper-detau=ils'>
+                    <div className='product-wrapper' data-aos="fade-right" data-aos-duration='100'>
+                      <div className='wrapper-img'>
+                        <img src={getImageBase() + "category/" + item.photo} alt="dress" />
+                      </div>
+                      <div className='wrapper-info'>
+                        {item.title}
+                      </div>
+                    </div>
+                  </Link>
+                )
+              })};
             </div>
           </div>
         </section>
@@ -35,5 +77,3 @@ class Shop extends React.Component {
     );
   }
 }
-
-export default Shop;
