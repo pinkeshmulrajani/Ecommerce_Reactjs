c716c1c0136ad1a62d383d3136636a2ba28c6f4c
pinkeshmulrajani <pinkeshmulrajani@gmail.com>
2026-08-21 12:01:11 +0530

    all routes is cookiesprovider in shop_users panel

--- shop_users/src/index.js
+++ shop_users/src/index.js
@@ -1,46 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Import Page Components
 import Home from './Home/home';
 import Shop from './Shop/shop';
+import Product from './Product/product';
+import ProductDetail from './Product/product_details';
+import Cart from './cart/cart';
+import Checkout from './Auth/checkout';
 import Login from './Auth/login';
 import Register from './Auth/register';
 import ChangePassword from './Auth/change_password';
 import ForgotPassword from './Auth/forgot_password';
-import Checkout from './Auth/checkout';
-import Category from './Category/category';
-import SiteCategoryProduct from './Category/site_category_product';
-import Product from './Product/product';
-import ProductDetails from './Product/product_details';
-import Cart from './cart/cart';
-
-export class MyRouter extends React.Component {
-  render() {
+import { CookiesProvider } from 'react-cookie';
+function MyRouter() {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/change-password" element={<ChangePassword />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/category-product" element={<SiteCategoryProduct />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/product-details" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </BrowserRouter>
+        <CookiesProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/product/:categoryid" element={<Product />} />
+                    <Route path="/product-detail/:productid" element={<ProductDetail />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/changepassword" element={<ChangePassword />} />
+                    <Route path="/forgotpassword" element={<ForgotPassword />} />
+                    <Route path="/shop" element={<Shop />} />
+                </Routes>
+            </Router>
+        </CookiesProvider>
     );
-  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <MyRouter />
-);
+root.render(<MyRouter />);
